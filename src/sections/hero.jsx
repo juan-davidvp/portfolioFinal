f23b9271925d38deb0493effcd1e2c9d6255943c
Juan David Velez Piedrahita <106885868+juan-davidvp@users.noreply.github.com>
2025-07-17 18:10:21 -0500

    final

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -16,62 +16,9 @@ const hero = () => {
 
         <div className='hero-layout'>
 
-        {/*LEFT: HERO CONTECT*/}
-        <header className='flex flex-col justify-center md:w-full w-screen md:px-44 px-8 md:pt-0 pt-14'>
-            <div className='flex flex-col gap-7'>
-                <div className='hero-text'>
-                    <h1 className='md:h-28 h-12 md:text-[94px] text-[40px]'>Multimedia</h1>
-                    <h1>
-                        <span className='slide md:pl-0.5 pl-0'>
-                            <span className='wrapper'>
-                                {words.map((word) => (
-                                    <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'>
-                                    <span>{word.text}</span>
-                                    <div className='xl:size-14 md:size-10 size-7 md:p-4 p-2 rounded-full bg-purple-50 center flex  items-center justify-center' >
-                                        <img 
-                                        src={word.imgPath} 
-                                        alt={word.text} 
-                                        
-                                    />
-                                    </div>
-                                    
-                                    
-                                    </span>
-                                ))}
-                            </span>
-                        </span>
-                    </h1>
-                    <p className="text-white md:text-xl text-xs relative z-10 pointer-events-none md:mt-30 mt-15 font-light">
-                        I build bridges between code and design to create digital experiences that not only work, but also fascinate. I am a full stack developer with the vision of a multimedia designer.
-                    </p>
-                    
-                </div>
-                <Button
-                    text="See My Work"
-                    className="md:w-80 md:h-16 w-60 h-12"
-                    id="counter"
-                    href="/assets/juanDavidVelezCV.pdf"
-                />
-
-                <div className="flex items-center justify-start gap-4 mt-2 ">
-                    <a href="https://www.instagram.com/juanvelezdp_/" target="_blank" rel="noopener noreferrer" className="social-button group">
-                        <div className="bg-circle" />
-                        <img src="/images/instagram.svg" alt="Instagram" />
-                    </a>
-                    <a href="https://x.com/juanvelezdp_" target="_blank" rel="noopener noreferrer" className="social-button group">
-                        <div className="bg-circle" />
-                        <img src="/images/x.svg" alt="X" />
-                    </a>
-                    <a href="https://co.linkedin.com/in/juan-david-velez-piedrahita-83389a347" target="_blank" rel="noopener noreferrer" className="social-button group">
-                        <div className="bg-circle" />
-                        <img src="/images/linkedin.svg" alt="LinkedIn" />
-                    </a>
-                </div>
-                
-            </div>
-        </header>
+        
         {/* RIGHT: 3D Model or Visual */}
-        <figure className="hero-visual-container md:pr-44 pr-8">
+        <figure className="hero-visual-container">
             <div className="  rounded-3xl p-4 md:p-6 flex-col-center gap-4 w-full max-w-[320px] md:max-w-[380px] border border-white/20  neon-effect">
             <div className="interactive-container ">
                 <HeroExperience />
@@ -96,6 +43,59 @@ const hero = () => {
             </div>
             
         </figure>
+
+        {/*LEFT: HERO CONTECT*/}
+        <header className='flex-col-center md:w-full w-screen'>
+            <div className='flex flex-col gap-7'>
+                <div className='hero-text'>
+                    <h1 className='md:h-28 h-12 md:text-[94px] text-[40px]'>Multimedia</h1>
+                    <h1>
+                        <span className='slide md:pl-0.5 pl-0'>
+                            <span className='wrapper'>
+                                {words.map((word) => (
+                                    <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'>
+                                    <span>{word.text}</span>
+                                    <div className='xl:size-14 md:size-10 size-7 md:p-4 p-2 rounded-full bg-purple-50 center flex  items-center justify-center' >
+                                        <img 
+                                        src={word.imgPath} 
+                                        alt={word.text} 
+                                        
+                                    />
+                                    </div>
+                                    </span>
+                                ))}
+                            </span>
+                        </span>
+                    </h1>
+                    <p className="text-white md:text-xl text-xs relative z-10 pointer-events-none md:mt-30 mt-15 font-light">
+                        I build bridges between code and design to create digital experiences that not only work, but also fascinate. I am a full stack developer with the vision of a multimedia designer.
+                    </p>
+                    
+                </div>
+                <Button
+                    text="See My Work"
+                    className="md:w-80 md:h-16 w-60 h-12"
+                    id="counter"
+                    href="/assets/juanDavidVelezCV.pdf"
+                />
+
+                <div className="flex items-center justify-start gap-4  ">
+                    <a href="https://www.instagram.com/juanvelezdp_/" target="_blank" rel="noopener noreferrer" className="social-button group">
+                        <div className="bg-circle" />
+                        <img src="/images/instagram.svg" alt="Instagram" />
+                    </a>
+                    <a href="https://x.com/juanvelezdp_" target="_blank" rel="noopener noreferrer" className="social-button group">
+                        <div className="bg-circle" />
+                        <img src="/images/x.svg" alt="X" />
+                    </a>
+                    <a href="https://co.linkedin.com/in/juan-david-velez-piedrahita-83389a347" target="_blank" rel="noopener noreferrer" className="social-button group">
+                        <div className="bg-circle" />
+                        <img src="/images/linkedin.svg" alt="LinkedIn" />
+                    </a>
+                </div>
+                
+            </div>
+        </header>
         </div>
     </section>
     )
